312d97b6c43688b9e59cbb529be20915ddcd1b17
enzo-mir <miraglioenzo93@gmail.com>
2023-11-07 02:58:18 +0100

    styles seperate

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import styled, { ThemeProvider } from "styled-components";
+import { ThemeProvider } from "styled-components";
 import { GlobalStyle } from "./styles/GlobalStyle";
 import { themeStore } from "./store/theme.store";
 import ListOfTodo from "./components/ListOfTodo";
@@ -6,70 +6,7 @@ import { todosStore } from "./store/todos_store";
 import darkTheme from "./styles/darkTheme";
 import lightTheme from "./styles/lightTheme";
 import CryptoJS from "crypto-js";
-
-const Wrapper = styled.main`
-  position: absolute;
-  top: 10vh;
-  left: 50%;
-  transform: translate(-50%, 0%);
-  display: grid;
-  grid-template-rows: auto 1fr;
-  width: clamp(400px, 40vw, 600px);
-  gap: 50px;
-
-  @media screen and (max-width: 900px) {
-    top: 0;
-    padding: 2em 1em;
-    width: 100%;
-    min-width: 0;
-    gap: 20px;
-  }
-
-  & > * {
-    width: 100%;
-  }
-
-  header {
-    display: flex;
-    justify-content: space-between;
-
-    h1 {
-      color: #fff;
-      letter-spacing: 0.5ch;
-    }
-  }
-
-  section {
-    position: relative;
-    display: flex;
-    flex-direction: column;
-    gap: 1.5em;
-
-    & > div {
-      display: flex;
-      gap: 20px;
-      background-color: ${(props) => props.theme.card_bg};
-      padding: 1.2em;
-      border-radius: 5px;
-
-      box-shadow: 0px -10px 50px -20px ${(props) => props.theme.shadow};
-
-      span {
-        width: 25px;
-        aspect-ratio: 1 / 1;
-        border: 1px solid ${(props) => props.theme.text_line_through};
-        border-radius: 50%;
-      }
-      input {
-        border: none;
-        background-color: transparent;
-        width: 100%;
-        outline: none;
-        color: ${(props) => props.theme.text_clr};
-      }
-    }
-  }
-`;
+import { Wrapper } from "./styles/AppStyle";
 
 function App() {
   const [currentTheme, setCurrentTheme] = themeStore((state) => [state.currentTheme, state.setCurrentTheme]);
--- src/components/ListOfTodo.tsx
+++ src/components/ListOfTodo.tsx
@@ -1,177 +1,8 @@
-import styled from "styled-components";
 import { todoType, todosStore, valueTodo } from "../store/todos_store";
 import deleteBtn from "../assets/images/icon-cross.svg";
 import { useEffect, useRef, useState } from "react";
 import CryptoJS from "crypto-js";
-
-const Wrapper = styled.article`
-  box-shadow: 0px 0px 30px -20px ${(props) => props.theme.shadow};
-  border-radius: 10px;
-  color: ${(props) => props.theme.text_clr};
-
-  aside {
-    > div {
-      display: flex;
-      align-items: center;
-      border-bottom: 1px solid ${(props) => props.theme.text_line_through};
-      gap: 20px;
-      background-color: ${(props) => props.theme.card_bg};
-      padding: 1.2em;
-    }
-    ul {
-      border-top-left-radius: 5px;
-      border-top-right-radius: 5px;
-      max-height: 50vh;
-      overflow-y: auto;
-
-      &::-webkit-scrollbar {
-        width: 10px;
-      }
-      &::-webkit-scrollbar-track {
-      }
-      &::-webkit-scrollbar-thumb {
-        background: ${(props) => props.theme.scrollbar};
-      }
-      &::-webkit-scrollbar-thumb:hover {
-        background-color: #fff;
-      }
-      & > li {
-        display: flex;
-        align-items: center;
-        border-bottom: 1px solid ${(props) => props.theme.text_line_through};
-        gap: 20px;
-        background-color: ${(props) => props.theme.card_bg};
-        padding: 1.2em;
-
-        & p {
-          width: 100%;
-        }
-        @media screen and (min-width: 900px) {
-          button {
-            transition: all 0.3s ease-out;
-            transform: translateY(-50%);
-            opacity: 0;
-            visibility: hidden;
-          }
-        }
-        @media screen and (max-width: 900px) {
-          button img {
-            width: 90%;
-          }
-        }
-        &:hover {
-          button {
-            transform: translateY(0%);
-            opacity: 1;
-            visibility: visible;
-          }
-        }
-        & > input {
-          display: none;
-        }
-        label {
-          position: relative;
-          display: grid;
-          place-items: center;
-          width: 25px;
-          aspect-ratio: 1 / 1;
-          outline: 1px solid ${(props) => props.theme.text_line_through};
-          border-radius: 50%;
-          &:hover {
-            cursor: pointer;
-          }
-        }
-        &:not(:has(input:checked)) label:hover {
-          outline: none;
-          background: ${(props) => props.theme.gradient_check};
-          &::after {
-            content: "";
-            position: absolute;
-            width: calc(100% - 2px);
-            aspect-ratio: 1 / 1;
-            background: ${(props) => props.theme.card_bg};
-            background-position: center;
-            background-size: 75%;
-            background-repeat: no-repeat;
-            border-radius: 50%;
-            z-index: 5;
-          }
-        }
-
-        &:has(input:checked) {
-          p {
-            text-decoration: line-through;
-            color: ${(props) => props.theme.text_line_through};
-          }
-          label {
-            background: ${(props) => props.theme.gradient_check};
-            outline: none;
-            &::after {
-              content: "";
-              position: absolute;
-              width: 100%;
-              height: 100%;
-              background: url(${(props) => props.theme.image_checker});
-              background-position: center;
-              background-size: 75%;
-              background-repeat: no-repeat;
-              border-radius: 50%;
-            }
-          }
-        }
-      }
-    }
-  }
-
-  footer {
-    position: relative;
-    display: flex;
-    padding: 1.2em;
-    justify-content: space-between;
-    background-color: ${(props) => props.theme.card_bg};
-    color: ${(props) => props.theme.text_line_through};
-
-    ul {
-      display: flex;
-      gap: 20px;
-      li {
-        list-style-type: none;
-        button {
-          color: ${(props) => props.theme.text_line_through};
-        }
-        &.current {
-          button {
-            color: ${(props) => props.theme.acitve_link};
-          }
-        }
-        &:hover {
-          cursor: grab;
-
-          &:not(&.current) {
-            button {
-              color: ${(props) => props.theme.active_link_hover};
-            }
-          }
-        }
-      }
-    }
-
-    @media screen and (max-width: 900px) {
-      ul:nth-child(2) {
-        position: absolute;
-        justify-content: center;
-        border-radius: 5px;
-        bottom: -150%;
-        left: 50%;
-        transform: translateX(-50%);
-        background: ${(props) => props.theme.card_bg};
-        width: 100%;
-        padding: 1em;
-        box-shadow: 0 0 15px -10px ${(props) => props.theme.shadow};
-      }
-    }
-  }
-`;
+import { Wrapper } from "../styles/ListOfTodosStyle";
 
 export default function ListOfTodo() {
   const [filter, setFilter] = useState<"all" | "active" | "completed">("all");
@@ -206,6 +37,7 @@ export default function ListOfTodo() {
     const encryptedData = CryptoJS.AES.encrypt(JSON.stringify(reorderedItems), "mysecretkey").toString();
     sessionStorage.setItem("session_todo", encryptedData);
   };
+
   function changeTodosInformation(todo: valueTodo, e: React.ChangeEvent | null, type: "delete" | "complete") {
     const obj = [...todos];
     for (let i = 0; i < obj.length; i++) {
@@ -219,6 +51,7 @@ export default function ListOfTodo() {
         }
       }
     }
+
     setTodos(obj);
     const encryptedData = CryptoJS.AES.encrypt(JSON.stringify(obj), "mysecretkey").toString();
     sessionStorage.setItem("session_todo", encryptedData);
